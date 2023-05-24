51158043b5d1fb7885ca7879c793e1f6537ea9d2
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2023-05-24 15:42:57 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
